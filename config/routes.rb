d4db0aa7c10b5ce9146eb76a78cb6ce962868a20
Amanda <ajnikrant@gmail.com>
2021-03-18 15:50:41 -0400

    wokring on it

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :employees
+#   resources :dogs
+#   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/dogs', to: 'dogs#index', as: '/dogs'
 end
